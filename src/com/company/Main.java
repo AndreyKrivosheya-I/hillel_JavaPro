76168b2c01a30d91cd6797e621c21f42768912a7
AndreyKrivosheya-I <115229701+AndreyKrivosheya-I@users.noreply.github.com>
2023-05-09 20:42:50 +0300

    Inheritance
    Task about one of paradigmas OOP "inheritance"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,16 @@
 package com.company;
+
 public class Main {
 
     public static void main(String[] args) {
-        Employee employee = new Employee("name", "post","email","number",35);
+   /*Cat catBarsik = new Cat();
+    catBarsik.run(34);
+    catBarsik.swim(10);
+    Dog dogBobik = new Dog();
+    dogBobik.run(150);
+    dogBobik.swim(20);
+    Animal animal = new Animal(); */
+
+    System.out.println("Общее количество котов: " + Cat.getCatCount() + "\nОбщее количество собак: " + Dog.getDogCount() + "\nОбщее количество остальных животных:  " + (Animal.getAnimalCount()-Dog.getDogCount()-Cat.getCatCount()) );
     }
 }
