ce49a1c3e89b10902f0acd1d1c491864d6b2fe70
AndreyKrivosheya-I <115229701+AndreyKrivosheya-I@users.noreply.github.com>
2023-05-09 20:54:39 +0300

    The second part of task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,29 @@
 package com.company;
 
+import com.company.obstacle.Obstacle;
+import com.company.obstacle.Road;
+import com.company.obstacle.Wall;
+import com.company.players.Cat;
+import com.company.players.Person;
+import com.company.players.Player;
+import com.company.players.Robot;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Player[] players = {new Cat("Барсик", 100., 0.),
+                new Person("Никита", 50., 10.),
+                new Robot("Омега", 1000., 100.)};
 
-        Figure[] figures = {new Circle(4), new Square(2), new Triangle(3,4,5)};
-        totalArea(figures);
+        Obstacle[] obstacles = {new Road(10.),
+                new Wall(10.),
+                new Road(75.)};
 
-        //Circle: 3.14 * 4 * 4 = 50.24
-        //Square: 2 * 2 = 4
-        //Triangle: 6
-        //total : 60.24
-    }
-    public static double totalArea(Figure[] figures){
-        double totalArea = 0;
-        for (int i = 0; i < figures.length; i++) {
-            totalArea += figures[i].area();
+        for (Player player : players){
+            for (Obstacle obstacle : obstacles){
+                if (!player.obstacle(obstacle)) break;
+            }
+            System.out.print("\n\n\n");
         }
-        System.out.println("Общая площадь фигур: " + totalArea);
-        return totalArea;
     }
 }
